57cce5ac90e9802b0089c3930c3320031ee8801d
210057zzh <davidz@usc.edu>
2021-11-06 18:15:06 -0700

    test

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,29 +7,30 @@ public class PlayerMove : MonoBehaviour
     private bool isMoving;
     private Vector3 origPos, targetPos;
     private float timeToMove = 0.2f;
+    float elapsedTime = 0;
 
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetAxisRaw("Vertical") == 1 && !isMoving)
-            StartCoroutine(MovePlayer(Vector3.up));
+            MovePlayer(Vector3.up);
 
         if (Input.GetAxisRaw("Horizontal") == -1 && !isMoving)
-            StartCoroutine(MovePlayer(Vector3.left));
+            MovePlayer(Vector3.left);
 
         if (Input.GetAxisRaw("Vertical") == -1 && !isMoving)
-            StartCoroutine(MovePlayer(Vector3.down));
+            MovePlayer(Vector3.down);
 
         if (Input.GetAxisRaw("Horizontal") == 1 && !isMoving)
-            StartCoroutine(MovePlayer(Vector3.right));
+            MovePlayer(Vector3.right);
     }
 
     private IEnumerator MovePlayer(Vector3 direction)
     {
         isMoving = true;
 
-        float elapsedTime = 0;
+        elapsedTime = 0;
 
         origPos = transform.position;
         targetPos = origPos + direction;
