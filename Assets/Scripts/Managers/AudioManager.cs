d4bc0c17f8b040ff0aa7d69baffc4fff01f59bdb
210057zzh <davidz@usc.edu>
2021-11-07 16:34:54 -0800

    sound

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -20,7 +20,6 @@ public class AudioManager : MonoBehaviour
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        if(!s.source.isPlaying)
             s.source.Play();
     }
 }
